34f53b8181c4dd9a460bfd84b68994cea4b051b6
AbdiCytus <abdiprayuda203@gmail.com>
2023-10-28 16:16:09 +0800

    Added new Project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const mode = document.getElementById("mode");
 const menu = document.querySelector(".menu");
 const headCanvas = document.querySelector(".head-canvas");
-const mainCanvas = document.querySelector(".main")
+const mainCanvas = document.querySelector(".main");
 
 // Hamburger Menu
 
@@ -35,11 +35,74 @@ const aboutTitle = document.querySelector(".about-title");
 
 const projects = document.querySelector(".projects");
 const projectsTitle = document.querySelector(".projects-title");
+
+// System
+
+// Card Loop
+
+for (let e = 0; e < 5; e++) {
+  const cardElement = document.createElement("div");
+  projects.appendChild(cardElement);
+  cardElement.setAttribute("class", `card card${e + 1}`);
+}
 const cards = document.getElementsByClassName("card");
+
+for (let e = 0; e < cards.length; e++) {
+  const element = cards[e];
+
+  const cardImages = document.createElement("div");
+  const cardText = document.createElement("div");
+
+  const loopImages = document.createElement("img");
+  const loopH3 = document.createElement("h3");
+  const loopP = document.createElement("p");
+  const loopBtn = document.createElement("div");
+
+  const loopLink = document.createElement("a");
+
+  element.appendChild(cardImages);
+  element.appendChild(cardText);
+
+  cardImages.appendChild(loopImages);
+  cardText.appendChild(loopH3);
+  cardText.appendChild(loopP);
+  cardText.appendChild(loopBtn);
+
+  loopBtn.appendChild(loopLink);
+
+  cardImages.setAttribute("class", "card-img");
+  cardText.setAttribute("class", "card-text");
+
+  loopImages.setAttribute("class", "img-project");
+  loopImages.setAttribute("src", "images/Gab Project.webp");
+
+  loopH3.innerHTML = `Project ${e + 1}`;
+  loopP.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  loopBtn.setAttribute("class", "card-btn");
+
+  loopLink.innerHTML = "View";
+  loopLink.style.color = "white";
+}
+
+const cardImages = document.getElementsByClassName("img-project");
 const cardsTitle = document.getElementsByClassName("card-text");
 const cardsbtn = document.getElementsByClassName("card-btn");
 
-// System
+// Data Projects
+const project1 = {
+  img: (cardImages[0].src = "images/Seele - HSR.webp"),
+  title: (cardsTitle[0].querySelector("h3").innerHTML = "Gacha Generator"),
+  desc: (cardsTitle[0].querySelector("p").innerHTML =
+    "Honkai: StarRail gacha generator fanmade by AbdiCytus."),
+  link: cardsbtn[0]
+    .querySelector("a")
+    .setAttribute("href", "https://hsr-gacha-generator.netlify.app"),
+  target: cardsbtn[0].querySelector("a").setAttribute("target", "_blank"),
+  adjust: {
+    button: (cardsTitle[0].querySelector("p").style.marginBottom = "10px"),
+  },
+};
 
 // Hamburger Menu
 
@@ -51,20 +114,20 @@ menu.onclick = () => {
 };
 
 headCanvas.onclick = () => {
-    if (nav.classList.contains("slide")) {
-        nav.classList.remove("slide");
-        span2.classList.remove("display-none");
-        span1.classList.remove("line-1");
-        span3.classList.remove("line-2");
+  if (nav.classList.contains("slide")) {
+    nav.classList.remove("slide");
+    span2.classList.remove("display-none");
+    span1.classList.remove("line-1");
+    span3.classList.remove("line-2");
   }
 };
 
 mainCanvas.onclick = () => {
-    if (nav.classList.contains("slide")) {
-        nav.classList.remove("slide");
-        span2.classList.remove("display-none");
-        span1.classList.remove("line-1");
-        span3.classList.remove("line-2");
+  if (nav.classList.contains("slide")) {
+    nav.classList.remove("slide");
+    span2.classList.remove("display-none");
+    span1.classList.remove("line-1");
+    span3.classList.remove("line-2");
   }
 };
 
@@ -75,9 +138,9 @@ mode.onclick = () => {
     mode.innerHTML = "NIGHT";
     bg.classList.add("dark-head");
     menu.style.backgroundColor = "#212121";
-    github.src = "images/github logo(white).png";
-    ig.src = "images/instagram logo(white).png";
-    email.src = "images/email logo(white).png";
+    github.src = "images/github logo(white).webp";
+    ig.src = "images/instagram logo(white).webp";
+    email.src = "images/email logo(white).webp";
     body.classList.add("pure-black");
     head.classList.add("black");
     nav.classList.add("black");
@@ -99,9 +162,9 @@ mode.onclick = () => {
     mode.innerHTML = "DAY";
     bg.classList.remove("dark-head");
     menu.style.backgroundColor = "#3f51b5";
-    github.src = "images/github logo.png";
-    ig.src = "images/instagram logo.png";
-    email.src = "images/email logo.png";
+    github.src = "images/github logo.webp";
+    ig.src = "images/instagram logo.webp";
+    email.src = "images/email logo.webp";
     body.classList.remove("pure-black");
     head.classList.remove("black");
     nav.classList.remove("black");
